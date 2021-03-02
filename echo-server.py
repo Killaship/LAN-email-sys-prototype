714adfdea70cef5ac16d5ceec042122cc6688701
Killaship <69988679+Killaship@users.noreply.github.com>
2021-03-02 15:50:26 -0500

    Update echo-server.py

--- echo-server.py
+++ echo-server.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = 'YOUR_IP'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -13,12 +13,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         while True:
              
-             string = input("Please enter email message.")
+             string = input("Please enter email message.")  #this message should eventually make it to the client program, whether it be over LAN or 2 separate programs on a computer.
 
              arr = bytes(str(string), 'utf-8')
-             conn.sendall(arr)
+             conn.sendall(arr) #sending the bytes from the string
              data = conn.recv(1024)
-             if not data:
+             if not data: #idk what this is for but it looks important
                  conn.sendall(data)
                  break
                 
