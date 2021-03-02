a507f3d09ecaa9b63ffffd8698bf4073d70a64bb
Killaship <69988679+Killaship@users.noreply.github.com>
2021-03-02 15:47:11 -0500

    Add files via upload

---
+++ echo-server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+             
+             string = input("Please enter email message.")
+
+             arr = bytes(str(string), 'utf-8')
+             conn.sendall(arr)
+             data = conn.recv(1024)
+             if not data:
+                 conn.sendall(data)
+                 break
+                
