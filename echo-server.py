21d26198dca043e533ab943931fcc5889e49b681
Killaship <69988679+Killaship@users.noreply.github.com>
2021-03-06 07:25:12 -0500

    Make the server ip be able to be configured from the console at runtime
    I'm a korok! i made the change so this thing actually works over LAN, prooftested it with a raspberry pi 0 W. did the same thing to the client. might be only 1 way sending from the raspberry pi bc firewall. P.S. The readme is now very outdated.

--- echo-server.py
+++ echo-server.py
@@ -1,24 +1,255 @@
 #!/usr/bin/env python3
+def server():
 
-import socket
 
-HOST = 'YOUR_IP'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+ 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-             
-             string = input("Please enter email message.")  #this message should eventually make it to the client program, whether it be over LAN or 2 separate programs on a computer.
-
-             arr = bytes(str(string), 'utf-8')
-             conn.sendall(arr) #sending the bytes from the string
-             data = conn.recv(1024)
-             if not data: #idk what this is for but it looks important
-                 conn.sendall(data)
+     import socket
+     import time
+     time.sleep(1)
+     print("Hello world!")
+     print("Terminal Based LAN Chat 1.3 (Server)")
+     hostip = str(input("Enter the host IP. Type 0 to use a hardcoded value. (Usually localhost.)"))
+     if(hostip == 0):
+         # Hardcoded value goes here.
+         HOST = "127.0.0.1"
+     else:
+         HOST = hostip
+     print("Waiting for client to connect...")
+     HOST = "127.0.0.1"
+     PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+         s.bind((HOST, PORT))
+         s.listen()
+         conn, addr = s.accept()
+     with conn:
+         print('Connected by', addr)
+         print("Tip: You can send up to 30 messages in a single session.")
+         print("If you don't want that, just try to send 'quit' as a message.")
+    
+        # I WAS BEING LAZY DON'T @ ME!
+        
+         while True:
+         #You can send up to 30 messages in a session.
+             #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
                  break
-                
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+             #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+             #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+             #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+             #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+             #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+            #things 
+             string = input("Please enter email message.")
+             if(string == 'quit'):
+                 break
+             else:
+                 arr = bytes(str(string), 'utf-8')
+                 conn.sendall(arr)
+             
+
+
+server()
+
+
+
+
+
+
+
+# Wow, you made it to the bottom of the world. This code has as many lines as the 1.2 version of VoltOS!
